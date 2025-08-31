44812cc2da3d769e9bc7aacddfdf880667ea27fc
Sunday Alabi <iamalabisunday@gmail.com>
2025-08-31 15:36:07 -0700

    first commit

--- src/Pages/Technology.jsx
+++ src/Pages/Technology.jsx
@@ -32,39 +32,39 @@ export default function Technology() {
           </div>
         </header>
         {/* Text and Explore Section */}
-        <section className="w-full lg:px-[0rem] md:px-[0rem] lg:w-[69.375rem] text-white flex flex-col gap-[1.5rem] justify-center items-center md:my-[2rem] ">
+        <section className="w-full lg:px-[0rem] md:px-[0rem] lg:w-[69.375rem] text-white flex flex-col gap-[0rem] md:gap-[1.5rem] justify-center items-center md:my-[2rem] my-[1rem]">
           {/* Heading text */}
           <div className="md:w-full w-fit flex md:justify-start justify-center lg:items-center md:gap-[1.5rem] gap-[0.8rem] md:text-[1.25rem] text-[1rem] font-barlowCondensed md:tracking-[0.1875rem] tracking-[0.15rem] font-200 md:px-[2rem]">
             <span className="text-white opacity-[0.25]">03</span>
             <p className="uppercase">Space Launch 101</p>
           </div>
           {/* -----------------------Main content Section------------------------------- */}
-          <div className="lg:min-w-[69.4rem] lg:max-h-[40rem] md:h-full min-h-[43.8rem] flex flex-col-reverse lg:justify-center items-center lg:gap-[1.5rem] lg:py-[3.5rem] lg:grid lg:grid-cols-2">
+          <div className="lg:min-w-[69.4rem] lg:max-h-[40rem] md:h-full lg:min-h-[43.8rem] mt-[4rem] md:mt-[0rem] flex flex-col-reverse lg:justify-center md:items-center justify-start items-start lg:gap-[1.5rem] lg:py-[3.5rem] lg:grid lg:grid-cols-2 md:w-full">
             {/* Technology Text content */}
-            <section className="lg:min-w-[33.7rem] flex flex-col lg:justify-center lg:items-center bg-green-900">
+            <section className="lg:min-w-[33.7rem] flex flex-col lg:justify-center lg:items-center ">
               {/* Text Content Section */}
-              <div className="w-full h-full lg:min-h-[38.4rem] min-h-[17.4rem] flex flex-col md:justify-center justify-start lg:items-center items-start pt-[2.5rem] lg:pt-[0rem] bg-yellow-700">
-                <div className="flex flex-col lg:flex-row gap-[0.7rem] lg:gap-[1rem] lg:w-full md:w-full font-bellefair font-[300] text-center lg:text-start bg-blue-900 justify-center items-center">
+              <div className="w-full h-full lg:min-h-[38.4rem] min-h-[17.4rem] flex flex-col md:justify-center justify-start lg:items-center items-start pt-[2.5rem] lg:pt-[0rem]">
+                <div className="flex flex-col lg:flex-row gap-[0.7rem] lg:gap-[0.3rem] lg:w-full md:w-full font-bellefair font-[300] text-center lg:text-start justify-center items-center">
                   {/* Navigation Section */}
                   <nav>
                     <ul className="flex lg:flex-col flex-row justify-between md:gap-[2rem] lg:gap-[0.5rem] gap-[1rem]">
                       <li>
                         <NavLink to="launch">
-                          <div className="space lg:w-[5rem] lg:h-[5rem] md:w-[3.5rem] md:h-[3.5rem] rounded-full border-1 border-primary flex items-center justify-center md:text-[2rem] font-bellefair font-[300] text-white text-[1.125rem] w-[2.5rem] h-[2.5rem]">
+                          <div className="space lg:w-[5rem] lg:h-[5rem] md:w-[3.5rem] md:h-[3.5rem] rounded-full border-1 border-[#ffffff63] flex items-center justify-center md:text-[2rem] font-bellefair font-[300] text-white text-[1.125rem] w-[2.5rem] h-[2.5rem]">
                             <p>1</p>
                           </div>
                         </NavLink>
                       </li>
                       <li>
                         <NavLink to="spaceport">
-                          <div className="space lg:w-[5rem] lg:h-[5rem] md:w-[3.5rem] md:h-[3.5rem] rounded-full border-1 border-primary flex items-center justify-center md:text-[2rem] font-bellefair font-[300] text-[1.125rem] w-[2.5rem] h-[2.5rem]">
+                          <div className="space lg:w-[5rem] lg:h-[5rem] md:w-[3.5rem] md:h-[3.5rem] rounded-full border-1 border-[#ffffff63] flex items-center justify-center md:text-[2rem] font-bellefair font-[300] text-[1.125rem] w-[2.5rem] h-[2.5rem]">
                             <p>2</p>
                           </div>
                         </NavLink>
                       </li>
                       <li>
                         <NavLink to="spaceCapsule">
-                          <div className="space lg:w-[5rem] lg:h-[5rem] md:w-[3.5rem] md:h-[3.5rem] rounded-full border-1 border-primary flex items-center justify-center md:text-[2rem] font-bellefair font-[300] text-[1.125rem] w-[2.5rem] h-[2.5rem]">
+                          <div className="space lg:w-[5rem] lg:h-[5rem] md:w-[3.5rem] md:h-[3.5rem] rounded-full border-1 border-[#ffffff63] flex items-center justify-center md:text-[2rem] font-bellefair font-[300] text-[1.125rem] w-[2.5rem] h-[2.5rem]">
                             <p>3</p>
                           </div>
                         </NavLink>
@@ -78,7 +78,7 @@ export default function Technology() {
             </section>
 
             {/* Photo Section */}
-            <div className="w-full lg:min-h-[38rem] min-h-[18rem] md:min-h-[25rem] bg-primary bg-[url(/technology/image-launch-vehicle-portrait.jpg)] bg-cover bg-center bg-no-repeat lg:text-[2rem] md:text-[1.5rem] uppercase text-[1.125rem] md:mt-[2rem] lg:mt-[0rem]"></div>
+            <div className="relative w-full lg:min-h-[38rem] min-h-[18.4rem] md:min-h-[25rem] bg-primary bg-[url(/technology/image-launch-vehicle-portrait.jpg)] bg-cover bg-center bg-no-repeat lg:text-[2rem] md:text-[1.5rem] uppercase text-[1.125rem] md:mt-[2rem] lg:mt-[0rem]"></div>
           </div>
         </section>
       </div>
--- src/component/TechnologyText.jsx
+++ src/component/TechnologyText.jsx
@@ -2,18 +2,16 @@ import { Children } from "react";
 
 export default function TechnologyText({ title, name, children }) {
   return (
-    <div className="md:mx-auto font-bellefair font-[300] text-center lg:text-start bg-red-900">
-      <p className="lg:text-[2rem] md:text-[1.5rem] uppercase text-[1.125rem] opacity-[0.5]">
+    <div className="md:mx-auto font-bellefair font-[300] text-center lg:text-start">
+      <p className="md:text-[1.5rem] uppercase text-[1.125rem] opacity-[0.5]">
         {title}
       </p>
       <h1 className="lg:clam(3.5rem, 4vw + 1rem, 2.5rem) md:text-[2.5rem] uppercase text-[1.5rem]">
         {name}
       </h1>
-      <div className="w-full font-barlow lg:text-[1.125rem] lg:leading-[2.025rem] leading-[1.6875rem] text-[1rem] md:text-[1rem] mt-[0.25rem] lg:mt-[0rem] text-secondaryBlue">
+      <div className="md:max-w-[32rem] max-w-[100%] font-barlow lg:text-[1.125rem] lg:leading-[2.025rem] leading-[1.6875rem] text-[1rem] md:text-[1rem] mt-[0.25rem] lg:mt-[0rem] text-secondaryBlue px-[1rem]">
         {children}
       </div>
     </div>
   );
 }
-
-//
